e04429de08fbc14c9be78973c055f15bfd56ee36
Azad Salam <azadsalam2611@gmail.com>
2021-02-17 22:05:10 -0800

    added allow render for gfenv

--- src/scenic/simulators/gfootball/rl_trainer.py
+++ src/scenic/simulators/gfootball/rl_trainer.py
@@ -33,44 +33,47 @@ def basic_training(scenario):
 
 def run_built_in_ai_game_with_rl_env(rl_env):
     rl_env.render()
-    _ = rl_env.reset()
 
-    while True:
-        o, r, d, _ = rl_env.step([football_action_set.action_shot]) #football_action_set.action_builtin_ai
-        if d: break
+    for _ in range(3):
+        _ = rl_env.reset()
+        input()
+        while True:
+            o, r, d, i = rl_env.step([football_action_set.action_shot]) #football_action_set.action_builtin_ai
+            print(r, d, i)
+            if d: break
 
 
 class GFScenicEnv(gym.Env):
     metadata = {'render.modes': ['human']}
 
-    def __init__(self, initial_scenario, gf_env_settings = {}):
+    def __init__(self, initial_scenario, gf_env_settings = {}, allow_render = False):
         super(GFScenicEnv, self).__init__()
 
         self.gf_env_settings = gf_env_settings
-
+        self.allow_render = allow_render
         #self.initial_scenario = initial_scenario
         self.scenario = initial_scenario
         self.create_new_simulation = True #if set, initialize_new_simulation, will create a new simulation object, otherwise it will just return
-        self.initialize_new_simulation()
+        self.initialize_new_simulation(allow_render)
 
         self.gf_gym_env = self.simulation.get_underlying_gym_env()
         # Define action and observation space
         self.action_space = self.gf_gym_env.action_space
         self.observation_space = self.gf_gym_env.observation_space
 
-    def initialize_new_simulation(self):
+    def initialize_new_simulation(self, render=False):
 
         #TODO: Why using create_new_simulation ???
-        #if not self.create_new_simulation:
-        #    return
+        if not self.create_new_simulation:
+            return
         # generate a scene from the current scenario
         self.scene, _ = scenic_helper.generateScene(self.scenario)
 
         # initialize a new simulation object
         self.simulation = GFootBallSimulation(scene=self.scene, settings={}, for_gym_env=True,
-                                              render=False, verbosity=1, gf_env_settings=self.gf_env_settings)
+                                              render=render, verbosity=1, gf_env_settings=self.gf_env_settings)
 
-        #self.create_new_simulation = False
+        self.create_new_simulation = False
 
     def reset(self):
         # Reset the state of the environment to an initial state
