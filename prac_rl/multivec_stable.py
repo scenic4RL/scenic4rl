cc3b1ab40df58ddcaf4e4e9faed15a00f66d3c21
Azad Salam <azadsalam2611@gmail.com>
2021-03-10 14:40:04 -0800

    stable baseline vectorization test

---
+++ prac_rl/multivec_stable.py
@@ -0,0 +1,58 @@
+# Filter tensorflow version warnings
+import os
+# https://stackoverflow.com/questions/40426502/is-there-a-way-to-suppress-the-messages-tensorflow-prints/40426709
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # or any {'0', '1', '2'}
+import warnings
+# https://stackoverflow.com/questions/15777951/how-to-suppress-pandas-future-warning
+warnings.simplefilter(action='ignore', category=FutureWarning)
+warnings.simplefilter(action='ignore', category=Warning)
+
+import logging
+
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+
+import gym
+
+
+from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv
+from stable_baselines3.common.evaluation import evaluate_policy
+
+
+def make_env(env_id, rank, seed=0):
+    """
+    Utility function for multiprocessed env.
+
+    :param env_id: (str) the environment ID
+    :param seed: (int) the inital seed for RNG
+    :param rank: (int) index of the subprocess
+    """
+
+    def _init():
+        env = gym.make(env_id)
+        # Important: use a different seed for each environment
+        env.seed(seed + rank)
+        return env
+
+
+    return _init
+
+from stable_baselines3.common.cmd_util import make_vec_env
+
+env_id = 'CartPole-v1'
+# The different number of processes that will be used
+PROCESSES_TO_TEST = [1, 2, 4]
+NUM_EXPERIMENTS = 3 # RL algorithms can often be unstable, so we run several experiments (see https://arxiv.org/abs/1709.06560)
+TRAIN_STEPS = 5000
+# Number of episodes for evaluation
+EVAL_EPS = 20
+n_procs = 2
+
+# We will create one environment to evaluate the agent on
+eval_env = gym.make(env_id)
+#make_vec_env(env_id, n_envs=n_procs, vec_env_cls=SubprocVecEnv, vec_env_kwargs=dict(start_method='spawn'))
+train_env = SubprocVecEnv([make_env(env_id, i) for i in range(n_procs)], start_method='spawn')
+print("env created")
+train_env.close()
+print("env closed")
