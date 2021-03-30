7da2648887049e30ab33349a4ae1d6a7df97c149
Azad Salam <azadsalam2611@gmail.com>
2021-03-29 19:48:47 -0700

    weird error in run_simulation.py

--- examples/gfootball/run_simulation.py
+++ examples/gfootball/run_simulation.py
@@ -16,9 +16,9 @@ def mean_reward_random_agent(env, num_trials=1):
             # env.render()
             total_r += reward
             if done:
-                print("Reset ", i)
                 obs = env.reset()
                 num_epi += 1
+
     return total_r / num_epi
 
 gf_env_settings = {
@@ -30,38 +30,18 @@ gf_env_settings = {
     "action_set": "v2",  # "default/v2"
 }
 from scenic.simulators.gfootball.rl_interface import GFScenicEnv
-num_trials = 1
+num_trials = 2
 VISUAL = False
 scenario_file = f"{cwd}/../../examples/gfootball/try.scenic"
 scenario = buildScenario(scenario_file)
 env = GFScenicEnv(initial_scenario=scenario, gf_env_settings=gf_env_settings, allow_render=VISUAL,
                   use_scenic_behavior_in_step=True)
 
-env.reset()
-
-done=False
-while not done:
-    action = env.action_space.sample()
-    obs, reward, done, info = env.step(action)
-    if done:
-        #env.close()
-        env.reset()
-
 print("Trials: ", num_trials)
-#print("behavior based agent performance: ", mean_reward_random_agent(env, num_trials=num_trials))
+print("behavior based agent performance: ", mean_reward_random_agent(env, num_trials=num_trials))
 gf_env_settings["action_set"] = "default"
-#env = GFScenicEnv(initial_scenario=scenario, allow_render=False, gf_env_settings=gf_env_settings)
-#print("random agent performance: ", mean_reward_random_agent(env, num_trials=num_trials))
-
-
-
-
-
-
-
-
-
-
+env = GFScenicEnv(initial_scenario=scenario, allow_render=False, gf_env_settings=gf_env_settings)
+print("random agent performance: ", mean_reward_random_agent(env, num_trials=num_trials))
 
 
 
