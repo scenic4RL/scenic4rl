986a91ab2bed5d45eeb65111615584ff34fb8a09
Ubuntu <ubuntu@ip-172-31-21-249.us-west-2.compute.internal>
2021-04-15 20:52:14 +0000

    baseline integration started

--- training/gfrl/common/test_model.py
+++ training/gfrl/common/test_model.py
@@ -79,11 +79,51 @@ def evalutate_agent(env, model, n_trials):
     print(evaluate_policy(model, eval_env, n_eval_episodes=5))
 
 
+
+def evalutate_random_policy(n_trials):
+    from gfrl.common.my_sb.my_evaluation import evaluate_policy
+    from gfrl.common import sb_utils
+    import os
+    scenario_file = f"{os.getcwd()}/../_scenarios/exp/pass_n_shoot.scenic"
+
+    gf_env_settings = {
+        "stacked": True,
+        "rewards": "scoring",
+        "representation": 'extracted',
+        "players": [f"agent:left_players=1"],
+        "real_time": False,
+        "action_set": "default",
+        "dump_full_episodes": False,
+        "dump_scores": False,
+        "write_video": False,
+        "tracesdir": "dummy",
+        "write_full_episode_dumps": False,
+        "write_goal_dumps": False,
+        "render": False
+    }
+
+    scenario = buildScenario(scenario_file)
+    env = GFScenicEnv(initial_scenario=scenario, gf_env_settings=gf_env_settings, rank=rank)
+
+    rews=[]
+    for _ in range(n_trials):
+        done = False
+        rew = 0
+        o = env.reset()
+        while not done:
+            action = env.action_space.sample()
+            o,r,done,_ = env.step(action)
+            rew += r 
+
+        rews.append(rew)
+
+    print(rews)
 from gfrl.common.my_sb.ppo import PPO
 from stable_baselines3.common.vec_env import VecTransposeImage, DummyVecEnv
 
 if __name__ == "__main__":
 
-    visualize_agent()
+    #visualize_agent()
+    evalutate_random_policy(5)
 
 
---
+++ training/gfrl/run_exp.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#OPENAI_LOG_FORMAT="stdout,tensorboard,log,csv"
+python3 -u -m run_my_ppo2 \
+  --level 11_vs_11_easy_stochastic \
+  --reward_experiment scoring \
+  --policy impala_cnn \
+  --cliprange 0.115 \
+  --gamma 0.997 \
+  --ent_coef 0.00155 \
+  --num_timesteps 10000 \
+  --max_grad_norm 0.76 \
+  --lr 0.00011879 \
+  --num_envs 8 \
+  --noptepochs 2 \
+  --nminibatches 8 \
+  --nsteps 512 \
+  --save_interval 2 \
+  --log_path dummy \
+  "$@"
+
+
+# Good but unsettable defaults:
+# Optimizer: adam
+# Value-function coefficient is 0.5
+# GAE (lam): 0.95
