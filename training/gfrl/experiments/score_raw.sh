4bd15699c6d4d2d5c9a5311c48780c60c0674430
Ubuntu <ubuntu@ip-172-31-27-170.us-west-2.compute.internal>
2021-04-20 01:47:04 +0000

    experiment on raw gf, run to score

--- training/gfrl/experiments/score_raw.sh
+++ training/gfrl/experiments/score_raw.sh
@@ -7,14 +7,14 @@ python3 -u -m gfrl.base.run_my_ppo2 \
   --cliprange 0.115 \
   --gamma 0.997 \
   --ent_coef 0.00155 \
-  --num_timesteps 18000 \
+  --num_timesteps 500000 \
   --max_grad_norm 0.76 \
   --lr 0.00011879 \
   --num_envs 16 \
   --noptepochs 2 \
   --nminibatches 4 \
   --nsteps 512 \
-  --save_interval 1 \
+  --save_interval 10 \
   --exp_root ../_res \
   --exp_name rts \
   --run_raw_gf
