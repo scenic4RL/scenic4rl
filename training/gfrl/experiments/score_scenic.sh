b7c4cdce0c1d02e6d59ebb4e5ac6aa8d331e298f
Azad <azadsalam2611@gmail.com>
2021-04-23 23:31:57 +0000

    New Exps

--- training/gfrl/experiments/score_scenic.sh
+++ training/gfrl/experiments/score_scenic.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 python3 -u -m gfrl.base.run_my_ppo2 \
-  --level ../_scenarios/academy/rts_with_keeper.scenic  \
-  --eval_level ../_scenarios/academy/rts_with_keeper.scenic  \
+  --level ../_scenarios/sc4rl/ps_3v2_0.scenic  \
+  --eval_level ../_scenarios/sc4rl/ps_3v2_0.scenic  \
   --reward_experiment scoring \
   --policy gfootball_impala_cnn \
   --cliprange 0.115 \
@@ -17,14 +17,14 @@ python3 -u -m gfrl.base.run_my_ppo2 \
   --nsteps 512 \
   --save_interval 10 \
   --eval_interval  5 \
-  --exp_root ../_academy_1M \
-  --exp_name rts_with_keeper \
+  --exp_root ../_resv2/sc4rl \
+  --exp_name pass_3v2_0 \
   "$@"
 
 
 python3 -u -m gfrl.base.run_my_ppo2 \
-  --level ../_scenarios/academy/3v1.scenic  \
-  --eval_level ../_scenarios/academy/3v1.scenic  \
+  --level ../_scenarios/sc4rl/ps_3v2_1.scenic  \
+  --eval_level ../_scenarios/sc4rl/ps_3v2_1.scenic  \
   --reward_experiment scoring \
   --policy gfootball_impala_cnn \
   --cliprange 0.115 \
@@ -39,127 +39,7 @@ python3 -u -m gfrl.base.run_my_ppo2 \
   --nsteps 512 \
   --save_interval 10 \
   --eval_interval  5 \
-  --exp_root ../_academy_1M \
-  --exp_name 3v1 \
+  --exp_root ../_resv2/sc4rl \
+  --exp_name pass_3v2_1 \
   "$@"
-
-
-python3 -u -m gfrl.base.run_my_ppo2 \
-  --level ../_scenarios/academy/easy_counter.scenic  \
-  --eval_level ../_scenarios/academy/easy_counter.scenic  \
-  --reward_experiment scoring \
-  --policy gfootball_impala_cnn \
-  --cliprange 0.115 \
-  --gamma 0.997 \
-  --ent_coef 0.00155 \
-  --num_timesteps 1000000 \
-  --max_grad_norm 0.76 \
-  --lr 0.00011879 \
-  --num_envs 16 \
-  --noptepochs 2 \
-  --nminibatches 4 \
-  --nsteps 512 \
-  --save_interval 10 \
-  --eval_interval  5 \
-  --exp_root ../_academy_1M \
-  --exp_name easy_counter \
-  "$@"
-
-
-python3 -u -m gfrl.base.run_my_ppo2 \
-  --level ../_scenarios/academy/pass_n_shoot.scenic  \
-  --eval_level ../_scenarios/academy/pass_n_shoot.scenic  \
-  --reward_experiment scoring \
-  --policy gfootball_impala_cnn \
-  --cliprange 0.115 \
-  --gamma 0.997 \
-  --ent_coef 0.00155 \
-  --num_timesteps 1000000 \
-  --max_grad_norm 0.76 \
-  --lr 0.00011879 \
-  --num_envs 16 \
-  --noptepochs 2 \
-  --nminibatches 4 \
-  --nsteps 512 \
-  --save_interval 10 \
-  --eval_interval  5 \
-  --exp_root ../_academy_1M \
-  --exp_name pass_n_shoot \
-  "$@"
-
-
-
-python3 -u -m gfrl.base.run_my_ppo2 \
-  --level ../_scenarios/academy/rts.scenic  \
-  --eval_level ../_scenarios/academy/rts.scenic  \
-  --reward_experiment scoring \
-  --policy gfootball_impala_cnn \
-  --cliprange 0.115 \
-  --gamma 0.997 \
-  --ent_coef 0.00155 \
-  --num_timesteps 1000000 \
-  --max_grad_norm 0.76 \
-  --lr 0.00011879 \
-  --num_envs 16 \
-  --noptepochs 2 \
-  --nminibatches 4 \
-  --nsteps 512 \
-  --save_interval 10 \
-  --eval_interval  5 \
-  --exp_root ../_academy_1M \
-  --exp_name rts \
-  "$@"
-
-
-python3 -u -m gfrl.base.run_my_ppo2 \
-  --level ../_scenarios/academy/run_pass_shoot.scenic  \
-  --eval_level ../_scenarios/academy/run_pass_shoot.scenic  \
-  --reward_experiment scoring \
-  --policy gfootball_impala_cnn \
-  --cliprange 0.115 \
-  --gamma 0.997 \
-  --ent_coef 0.00155 \
-  --num_timesteps 1000000 \
-  --max_grad_norm 0.76 \
-  --lr 0.00011879 \
-  --num_envs 16 \
-  --noptepochs 2 \
-  --nminibatches 4 \
-  --nsteps 512 \
-  --save_interval 10 \
-  --eval_interval  5 \
-  --exp_root ../_academy_1M \
-  --exp_name run_pass_shoot \
-  "$@"
-
-
-python3 -u -m gfrl.base.run_my_ppo2 \
-  --level ../_scenarios/academy/rts_with_keeper.scenic  \
-  --eval_level ../_scenarios/academy/rts_with_keeper.scenic  \
-  --reward_experiment scoring \
-  --policy gfootball_impala_cnn \
-  --cliprange 0.115 \
-  --gamma 0.997 \
-  --ent_coef 0.00155 \
-  --num_timesteps 5000000 \
-  --max_grad_norm 0.76 \
-  --lr 0.00011879 \
-  --num_envs 16 \
-  --noptepochs 2 \
-  --nminibatches 4 \
-  --nsteps 512 \
-  --save_interval 50 \
-  --eval_interval 25 \
-  --exp_root ../_academy_5M \
-  --exp_name rts_with_keeper \
-  "$@"
-
-
-
-# Needed to add: max_grad_norm
-
-# Good but unsettable defaults:
-# Optimizer: adam
-# Value-function coefficient is 0.5
-# GAE (lam): 0.95
 
