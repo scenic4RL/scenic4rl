8f0707ebeaba12a63e20c5cefdb97d3c9ba0ad97
Azad <azadsalam2611@gmail.com>
2021-04-27 20:29:21 +0000

    log batch size

--- training/gfrl/experiments/bc.sh
+++ training/gfrl/experiments/bc.sh
@@ -1,25 +1,17 @@
 python3 -u -m gfrl.base.run_bc \
   --level ../_scenarios/academy/pass_n_shoot.scenic  \
   --eval_level ../_scenarios/academy/pass_n_shoot.scenic  \
-  --dataset /home/ubuntu/ScenicGFootBall/training/gfrl/_data/pns_10000.npz  \
+  --dataset /home/ubuntu/ScenicGFootBall/training/gfrl/_data/pns_rl_500.npz  \
   --reward_experiment scoring \
   --policy gfootball_impala_cnn \
   --lr 3e-4 \
-  --n_epochs 5 \
-  --batch_size 512 \
+  --n_epochs 2 \
+  --batch_size 256 \
   --save_interval  50 \
   --eval_timesteps 500 \
-  --eval_interval  5 \
+  --eval_interval  1 \
   --exp_root ../_res_bc \
-  --exp_name bc_pass_n_shoot_10000 \
+  --exp_name bc_pass_n_shoot_RL_16k_b256 \
   "$@"
-
-# Needed to add: max_grad_norm
-
-# Good but unsettable defaults:
-# Optimizer: adam
-# Value-function coefficient is 0.5
-# GAE (lam): 0.95
-
 
 
