a78f6f5b7694d21186adee24003d74fb2afb92d2
Azad <azadsalam2611@gmail.com>
2021-04-27 18:50:15 +0000

    full_game

--- training/gfrl/experiments/scenic_1M.sh
+++ training/gfrl/experiments/scenic_1M.sh
@@ -1,6 +1,49 @@
 #!/bin/bash
 
 python3 -u -m gfrl.base.run_my_ppo2 \
+  --level ../_scenarios/sc4rl/ps_3v2_1.scenic  \
+  --eval_level ../_scenarios/sc4rl/ps_3v2_1.scenic  \
+  --reward_experiment scoring \
+  --policy gfootball_impala_cnn \
+  --cliprange 0.115 \
+  --gamma 0.997 \
+  --ent_coef 0.00155 \
+  --num_timesteps 1000000 \
+  --max_grad_norm 0.76 \
+  --lr 0.00011879 \
+  --num_envs 16 \
+  --noptepochs 2 \
+  --nminibatches 4 \
+  --nsteps 512 \
+  --save_interval 10 \
+  --eval_interval 10 \
+  --exp_root ../_resv2/sc4rl1M \
+  --exp_name pass_3v2_1 \
+  "$@"
+
+
+python3 -u -m gfrl.base.run_my_ppo2 \
+  --level ../_scenarios/sc4rl/fg_11v1.scenic  \
+  --eval_level ../_scenarios/sc4rl/fg_11v1.scenic  \
+  --reward_experiment scoring \
+  --policy gfootball_impala_cnn \
+  --cliprange 0.115 \
+  --gamma 0.997 \
+  --ent_coef 0.00155 \
+  --num_timesteps 1000000 \
+  --max_grad_norm 0.76 \
+  --lr 0.00011879 \
+  --num_envs 16 \
+  --noptepochs 2 \
+  --nminibatches 4 \
+  --nsteps 512 \
+  --save_interval 10 \
+  --eval_interval 10 \
+  --exp_root ../_resv2/sc4rl1M \
+  --exp_name fg_11v1_v0 \
+  "$@"
+
+python3 -u -m gfrl.base.run_my_ppo2 \
   --level ../_scenarios/sc4rl/tp_3v3_v0.scenic  \
   --eval_level ../_scenarios/sc4rl/tp_3v3_v0.scenic  \
   --reward_experiment scoring \
@@ -17,7 +60,7 @@ python3 -u -m gfrl.base.run_my_ppo2 \
   --nsteps 512 \
   --save_interval 10 \
   --eval_interval 10 \
-  --exp_root ../_resv2/sc4rl \
+  --exp_root ../_resv2/sc4rl1M \
   --exp_name pass_3v2_0 \
   "$@"
 
@@ -38,7 +81,7 @@ python3 -u -m gfrl.base.run_my_ppo2 \
   --nsteps 512 \
   --save_interval 10 \
   --eval_interval 10 \
-  --exp_root ../_resv2/sc4rl \
+  --exp_root ../_resv2/sc4rl1M \
   --exp_name run_shoot_3v3_v0 \
   "$@"
 
@@ -59,8 +102,8 @@ python3 -u -m gfrl.base.run_my_ppo2 \
   --nsteps 512 \
   --save_interval 50 \
   --eval_interval 50 \
-  --exp_root ../_resv2/sc4rl \
-  --exp_name pass_3v2_0_5M \
+  --exp_root ../_resv2/sc4rl5M \
+  --exp_name pass_3v2_0 \
   "$@"
 
 
@@ -81,6 +124,6 @@ python3 -u -m gfrl.base.run_my_ppo2 \
   --nsteps 512 \
   --save_interval 50 \
   --eval_interval 50 \
-  --exp_root ../_resv2/sc4rl \
-  --exp_name pass_3v2_1_5M \
+  --exp_root ../_resv2/sc4rl5M \
+  --exp_name pass_3v2_1 \
   "$@"
