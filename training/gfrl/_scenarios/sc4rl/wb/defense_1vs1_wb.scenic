865bf023a32b5eac7ef062be959a5ec53b5ec3a1
Azad Salam <azadsalam2611@gmail.com>
2021-05-19 20:29:22 -0700

    defense scenarios added

---
+++ training/gfrl/_scenarios/sc4rl/wb/defense_1vs1_wb.scenic
@@ -0,0 +1,19 @@
+from scenic.simulators.gfootball.model import *
+from scenic.simulators.gfootball.behaviors import *
+from scenic.simulators.gfootball.simulator import GFootBallSimulator
+from scenic.core.geometry import normalizeAngle
+
+param game_duration = 400
+param deterministic = False
+param offsides = False
+param end_episode_on_score = True
+param end_episode_on_out_of_play = True
+param end_episode_on_possession_change = True
+
+MyGK at 95 @ 40, with behavior IdleBehavior()
+yellow_defender = MyRB
+
+OpGK at 95 @ 40, with behavior IdleBehavior()
+ego = OpAM on LeftReg_CM
+
+Ball ahead of ego by 0.5
--- training/gfrl/experiments/gen_demonstration.py
+++ training/gfrl/experiments/gen_demonstration.py
@@ -1,10 +1,9 @@
 from gfrl.base.bc.utils import generate_expert_successful_data
 
 
-
-scenario = "/Users/azadsalam/codebase/scenic/training/gfrl/_scenarios/academy/wb/3v1_wb_mark.scenic"
+scenario = "/Users/azadsalam/codebase/scenic/training/gfrl/_scenarios/academy/wb/pass_n_shoot_wb_mark.scenic"
 num_interactions = 10000
-data_path = "/Users/azadsalam/codebase/scenic/training/gfrl/_data/test"
+data_path = "/Users/azadsalam/codebase/scenic/training/gfrl/_data/academy_pass_n_shoot_mark_0"
 
 expert_observations, acts_oh, expert_rewards = generate_expert_successful_data(scenario_file=scenario, num_interactions=num_interactions, file_name=data_path)
 print("#"*80)
