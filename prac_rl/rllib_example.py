d38c63329ea98c7c083ed9c50c28bb32a5567c9f
Abdus Salam Azad <azadsalam2611@gmail.com>
2021-06-07 00:17:23 -0700

    cleaned unnecessary file

--- prac_rl/rllib_example.py
+++
@@ -1,5 +0,0 @@
-from ray import tune
-from ray.rllib.agents.ppo import PPOTrainer
-tune.run(PPOTrainer, config={"env": "CartPole-v0", "framework": "torch"})  # "log_level": "INFO" for verbose,
-                                                     # "framework": "tfe"/"tf2" for eager,
-                                                     # "framework": "torch" for PyTorch
--- training/gfrl/embed/utils.py
+++
@@ -1,2 +0,0 @@
-from collections import namedtuple
-Trajectory = namedtuple('Trajectory', ["obs_arr", "gt_arr", "act_arr", "rew_arr", "info_arr"])
