48ebbee88f18eff740bfbcf7c01e40eab83b62a7
Mark Wu <qcwu@berkeley.edu>
2022-02-28 07:11:37 +0000

    working hyperparameters

--- training/gfrl/experiments/run_multirl.py
+++ training/gfrl/experiments/run_multirl.py
@@ -85,7 +85,7 @@ class RllibGFootball(MultiAgentEnv):
 # running exps
 parser = argparse.ArgumentParser()
 
-parser.add_argument('--scenario-file', type=str, default="/home/mark/workplace/gf/scenic4rl/training/gfrl/_scenarios/grf/run_pass_shoot.scenic")
+parser.add_argument('--scenario-file', type=str, default="/home/qcwu/gf/scenic4rl/training/gfrl/_scenarios/grf/pass_n_shoot.scenic")
 parser.add_argument('--num-agents', type=int, default=2)
 parser.add_argument('--num-policies', type=int, default=2)
 parser.add_argument('--num-steps', type=int, default=5000000)
@@ -120,22 +120,59 @@ if __name__ == '__main__':
     rl_trainer_config = {
         'env': 'gfootball',
         "framework": "torch",
-        'lambda': 0.95,
-        'kl_coeff': 0.27,
-        'clip_rewards': False,
-        'vf_clip_param': 10.0,
-        'entropy_coeff': 0.01,
-        'train_batch_size': 1000,
-        # 'sample_batch_size': 100,
-        'sgd_minibatch_size': 500,
-        'num_sgd_iter': 10,
-        'num_workers': 1,
-        'num_envs_per_worker': 1,
-        'batch_mode': 'truncate_episodes',
-        'observation_filter': 'NoFilter',
-        'vf_share_layers': 'true',
+        # Discount factor of the MDP.
+        "gamma": 0.993,
+        # Should use a critic as a baseline (otherwise don't use value baseline;
+        # required for using GAE).
+        "use_critic": True,
+        # If true, use the Generalized Advantage Estimator (GAE)
+        # with a value function, see https://arxiv.org/pdf/1506.02438.pdf.
+        "use_gae": True,
+        # The GAE (lambda) parameter.
+        "lambda": 0.95,
+        # Initial coefficient for KL divergence.
+        "kl_coeff": 0.2,
+        # Size of batches collected from each worker.
+        "rollout_fragment_length": 200,
+        # Number of timesteps collected for each SGD round. This defines the size
+        # of each SGD epoch.
+        "train_batch_size": 4000,
+        # Total SGD batch size across all devices for SGD. This defines the
+        # minibatch size within each epoch.
+        "sgd_minibatch_size": 1000,
+        # Whether to shuffle sequences in the batch when training (recommended).
+        "shuffle_sequences": True,
+        # Number of SGD iterations in each outer loop (i.e., number of epochs to
+        # execute per train batch).
+        "num_sgd_iter": 10,
+        # Stepsize of SGD. 2.5e-4
+        "lr": 0.00008,
+        # Learning rate schedule.
+        "lr_schedule": None,
+        # Coefficient of the value function loss. IMPORTANT: you must tune this if
+        # you set vf_share_layers=True inside your model's config.
+        "vf_loss_coeff": 0.5,
+        # Coefficient of the entropy regularizer.
+        "entropy_coeff": 0.01,
+        # Decay schedule for the entropy regularizer.
+        "entropy_coeff_schedule": None,
+        # PPO clip parameter.
+        "clip_param": 0.27,
+        # Clip param for the value function. Note that this is sensitive to the
+        # scale of the rewards. If your expected V is large, increase this.
+        "vf_clip_param": 10.0,
+        # If specified, clip the global norm of gradients by this amount.
+        "grad_clip": 0.5,
+        # Target value for KL divergence.
+        "kl_target": 0.01,
+        # Whether to rollout "complete_episodes" or "truncate_episodes".
+        "batch_mode": "truncate_episodes",
+        # Which observation filter to apply to the observation.
+        "observation_filter": "NoFilter",
+
         'num_gpus': 1,
-        'lr': 0.00008,
+        'num_workers': 15,
+        'num_envs_per_worker': 1,
         'log_level': 'INFO',
         'simple_optimizer': args.simple,
         # All model-related settings go into this sub-dict.
@@ -159,7 +196,7 @@ if __name__ == '__main__':
 
     tune.run(
         'PPO',
-        name="285_passshoot_2left",
+        name="test_passshoot_2left_naturecnn",
         stop={'timesteps_total': args.num_steps},
         checkpoint_freq=50,
         config=rl_trainer_config,
