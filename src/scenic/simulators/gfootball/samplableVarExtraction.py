dcd2287ba93e6c851cc86e9a3a712f8c74d2510b
Mark Wu <qcwu@berkeley.edu>
2022-04-01 08:38:08 +0000

    remove bug

--- src/scenic/simulators/gfootball/samplableVarExtraction.py
+++ src/scenic/simulators/gfootball/samplableVarExtraction.py
@@ -130,22 +130,22 @@ def inputVarToScenario(scenario, inputDict):
 		conditionInputVar(obj, inputDict)
 	return None
 
-scenic_file = '/home/ek65/Desktop/scenic4rl/training/gfrl/_scenarios/defense/2vs2_with_scenic_high_pass_forward.scenic'
-scenario = scenic.scenarioFromFile(scenic_file)
+# scenic_file = '/home/ek65/Desktop/scenic4rl/training/gfrl/_scenarios/defense/2vs2_with_scenic_high_pass_forward.scenic'
+# scenario = scenic.scenarioFromFile(scenic_file)
 
-samplableVars = parseSamplableVars(scenario)
-varRanges = parseVar(samplableVars)
-print("varRanges: ", varRanges)
+# samplableVars = parseSamplableVars(scenario)
+# varRanges = parseVar(samplableVars)
+# print("varRanges: ", varRanges)
 
-sampledVars = randomSampleVars(varRanges)
-print("sampledVars: ", sampledVars)
-inputDict = createInputDictionary(samplableVars, sampledVars)
+# sampledVars = randomSampleVars(varRanges)
+# print("sampledVars: ", sampledVars)
+# inputDict = createInputDictionary(samplableVars, sampledVars)
 
-inputVarToScenario(scenario, inputDict)
-scene = scenario.generate()
+# inputVarToScenario(scenario, inputDict)
+# scene = scenario.generate()
 
-print("scene")
-print(scene.objects[0])
+# print("scene")
+# print(scene.objects[0])
 
 # print("scene: ", scene)
 # for obj in scene[0].objects:
