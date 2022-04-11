27dfa874dfce247a1fd0269cf0c923f2a600dcb1
Mark Wu <qcwu@berkeley.edu>
2022-04-11 04:16:18 +0000

    naming

--- training/gfrl/experiments/run_multirl.py
+++ training/gfrl/experiments/run_multirl.py
@@ -142,7 +142,7 @@ if __name__ == '__main__':
 
     tune.run(
         'PPO',
-        name=f"aaai2_{args.scenario}_{args.mode}_{args.id}",
+        name=f"aaai{args.id}_{args.scenario}_{args.mode}",
         stop={'timesteps_total': args.num_steps},
         checkpoint_freq=50,
         config=rl_trainer_config,
